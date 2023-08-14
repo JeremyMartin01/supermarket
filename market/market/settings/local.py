9048006109f0638bc2cd19047b30eca9c93f7cf7
Jeremías <martinbernardinoj@gmail.com>
2023-08-14 00:11:09 -0600

    Guardando cambios de sistema marketDJ 08/14/2023

---
+++ market/market/settings/local.py
@@ -0,0 +1,27 @@
+from .base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_secret('DB_NAME'),
+        'USER': get_secret('USER'),
+        'PASSWORD': get_secret('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
